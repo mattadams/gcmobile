mattadams/gcmobile

commit 11b6b286c6644b3eb1a45c6feac728f909b9710a

Merged intent of changeset into our codestream
changeset:   402:3a849e292c7c
branch:      v1.1.5
user:        carlhartung
date:        Tue Oct 19 00:15:30 2010 -0700
files: src/org/odk/collect/android/activities/FormEntryActivity.java
src/org/odk/collect/android/activities/FormHierarchyActivity.java
description:
fixes bug where formentrycontroller and UI can get out of sync, causing invalid answers to be saved to data model.

src/com/radicaldynamic/turboform/activities/FormHierarchyList.java
@@ -127,7 +127,7 @@ public class FormHierarchyList extends ListActivity {
             @Override
             public void onClick(View v) {
                 mFormEntryController.jumpToIndex(FormIndex.createBeginningOfFormIndex());
-                setResult(RESULT_CANCELED);
+                setResult(RESULT_OK);
                 finish();
             }
         });
@@ -137,7 +137,7 @@ public class FormHierarchyList extends ListActivity {
             @Override
             public void onClick(View v) {
                 mFormEntryController.jumpToIndex(FormIndex.createEndOfFormIndex());
-                setResult(RESULT_CANCELED);
+                setResult(RESULT_OK);
                 finish();
             }
         });
@@ -181,12 +181,12 @@ public class FormHierarchyList extends ListActivity {
                 break;
             case QUESTION:
                 mFormEntryController.jumpToIndex(h.getFormIndex());
-                setResult(RESULT_CANCELED);
+                setResult(RESULT_OK);
                 finish();
                 return;
             case CHILD:
                 mFormEntryController.jumpToIndex(h.getFormIndex());
-                setResult(RESULT_CANCELED);
+                setResult(RESULT_OK);
                 refreshView();
                 return;
         }

src/com/radicaldynamic/turboform/activities/FormEntryActivity.java
@@ -91,7 +91,8 @@ public class FormEntryActivity extends Activity implements AnimationListener,
     public static final int AUDIO_CAPTURE = 3;
     public static final int VIDEO_CAPTURE = 4;
     public static final int LOCATION_CAPTURE = 5;
-    public static final int HIERARCHY_BROWSER = 6;                  // Navigate to another instance (via FormHierarchyActivity)
+    //public static final int HIERARCHY_ACTIVITY = 6;               // Kept to make sense of intent of upstream changeset 402
+    public static final int HIERARCHY_BROWSER = 7;                  // Navigate to another instance (via FormHierarchyActivity)
 
     public static final String LOCATION_RESULT = "LOCATION_RESULT";
     
@@ -323,18 +324,6 @@ public class FormEntryActivity extends Activity implements AnimationListener,
             return;
     
         switch (requestCode) {
-        case HIERARCHY_BROWSER:
-            if (intent.getAction().equals("next_instance")) {
-                browseToNextInstance();
-            } else if (intent.getAction().equals("previous_instance")) {
-                browseToPreviousInstance();
-            } else if (intent.getAction().equals("return_to_browser")) {
-                discardChangesAndExit();
-            } else {
-                // Throw exception?
-            }            
-            break;
-            
         case BARCODE_CAPTURE:
             String sb = intent.getStringExtra("SCAN_RESULT");
             ((AbstractFolioView) mCurrentView).setBinaryData(sb);
@@ -379,6 +368,19 @@ public class FormEntryActivity extends Activity implements AnimationListener,
             ((AbstractFolioView) mCurrentView).setBinaryData(sl);
             saveCurrentAnswer(false);
             break;
+            
+        case HIERARCHY_BROWSER:
+            if (intent.getAction().equals("next_instance")) {
+                browseToNextInstance();
+            } else if (intent.getAction().equals("previous_instance")) {
+                browseToPreviousInstance();
+            } else if (intent.getAction().equals("return_to_browser")) {
+                discardChangesAndExit();
+            } else {
+                // We may have jumped to a new index in hierarchy activity, so refresh
+                refreshCurrentView();
+            }            
+            break;            
         }
     }
 

commit 772e3c2cc9f0333759243d7a459b73e1726ee3f3

Fixed bug introduced by merge of changeset 402 (bug introduced in 11b6b286c6644b3eb1a45c6feac728f909b9710a)

src/com/radicaldynamic/turboform/activities/FormHierarchyList.java
@@ -127,7 +127,7 @@ public class FormHierarchyList extends ListActivity {
             @Override
             public void onClick(View v) {
                 mFormEntryController.jumpToIndex(FormIndex.createBeginningOfFormIndex());
-                setResult(RESULT_OK);
+                setResult(RESULT_OK, (new Intent()).setAction("nothing"));
                 finish();
             }
         });
@@ -137,7 +137,7 @@ public class FormHierarchyList extends ListActivity {
             @Override
             public void onClick(View v) {
                 mFormEntryController.jumpToIndex(FormIndex.createEndOfFormIndex());
-                setResult(RESULT_OK);
+                setResult(RESULT_OK, (new Intent()).setAction("nothing"));
                 finish();
             }
         });
@@ -181,12 +181,12 @@ public class FormHierarchyList extends ListActivity {
                 break;
             case QUESTION:
                 mFormEntryController.jumpToIndex(h.getFormIndex());
-                setResult(RESULT_OK);
+                setResult(RESULT_OK, (new Intent()).setAction("nothing"));
                 finish();
                 return;
             case CHILD:
                 mFormEntryController.jumpToIndex(h.getFormIndex());
-                setResult(RESULT_OK);
+                setResult(RESULT_OK, (new Intent()).setAction("nothing"));
                 refreshView();
                 return;
         }