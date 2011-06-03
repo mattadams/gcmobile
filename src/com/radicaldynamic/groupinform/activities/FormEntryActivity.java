e80ccb301133238396a894ed27bc06f4dfa5457f
Matt Adams/Radical Dynamic Inc <matt.adams@radicaldynamic.com>
2011-06-02 18:01:29 -0600

    Merge upstream changeset as-is
    changeset:   566:a9d383740f29
    tag:         tip
    user:        carlhartung
    date:        Wed Jun 01 15:54:21 2011 -0700
    description:
    adding StringNumber widget.  Also removing random debugging statements.

--- src/com/radicaldynamic/groupinform/activities/FormEntryActivity.java
+++ src/com/radicaldynamic/groupinform/activities/FormEntryActivity.java
@@ -1142,12 +1142,6 @@ public class FormEntryActivity extends Activity implements AnimationListener, Fo
             return false;
         }
 
-        Log.e("Carl", "what?  intent? " + getIntent());
-        if (getIntent() != null) {
-            Log.e("Carl", "what?  data? " + getIntent().getData());
-        } else {
-            Log.e("Carl", "but no data");
-        }
         mSaveToDiskTask = new SaveToDiskTask(getIntent().getData());
         mSaveToDiskTask.setFormSavedListener(this);
         mSaveToDiskTask.setExportVars(exit, complete);
